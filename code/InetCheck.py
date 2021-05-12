4ef402cf2d8561df467bec38a571604134bf5616
Andreas Menzel <mail@andreas-menzel.com>
2021-05-12 23:53:17 +0200

    Added argument parser

--- code/InetCheck.py
+++ code/InetCheck.py
@@ -1,14 +1,68 @@
+import argparse
 import socket
 import time
 
+
+# Setup parser
+parser = argparse.ArgumentParser(description='Check and monitor the internet connection')
+parser.add_argument('-H', '--host', help='unknown')
+parser.add_argument('-p', '--port', type=int, help='unknown')
+parser.add_argument('-t', '--timeout', type=int, help='unknown')
+parser.add_argument('-i', '--interval', type=int, help='unknown')
+parser.add_argument('-lf', '--log_file', help='unknown')
+parser.add_argument('-cc', '--cmd_connected', help='unknown')
+parser.add_argument('-cd', '--cmd_disconnected', help='unknown')
+args = parser.parse_args()
+
+
+# Define variables and initialize with default values
+conn_host = "example.com"
+conn_port = 80
+conn_timeout = 3
+
+check_interval = 5
+log_file = ""
+
+cmd_connected = ""
+cmd_disconnected = ""
+
+
+# Update variables according to program parameters
+if args.host != None:
+    conn_host = args.host
+    print("Using host " + conn_host)
+if args.port != None:
+    conn_port = args.port
+    print("Using port " + conn_port)
+if args.timeout != None:
+    conn_timeout = args.timeout
+    print("Using timeout " + conn_timeout + "s")
+
+if args.interval != None:
+    check_interval = args.interval
+    print("Using interval " + check_interval + "s")
+
+if args.log_file != None:
+    log_file = args.log_file
+    print("Using log-file " + log_file)
+
+if args.cmd_connected != None:
+    cmd_connected = args.cmd_connected
+    print("Command when connected: " + cmd_connected)
+if args.cmd_disconnected != None:
+    cmd_disconnected = args.cmd_disconnected
+    print("Command when disconnected: " + cmd_disconnected)
+
+
 # Check the internet connection
-def check_connection(host="example.com", port=80, timeout=3):
+def check_connection(host=conn_host, port=conn_port, timeout=conn_timeout):
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
         return True
     except socket.error as ex:
         return False
+
 
 # Convert seconds into readable string
 def passed_time(t_seconds):
@@ -23,13 +77,16 @@ def passed_time(t_seconds):
     return_string += str(seconds) + "s"
     return return_string
 
+
 def time_pretty(timestamp):
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
+
 
 # Declare and initialize variables
 connection_status = check_connection()
 connection_status_old = connection_status
 time_status_change = time.time()
+
 
 # Show first connection status
 if check_connection():
@@ -64,7 +121,7 @@ def main():
             print(time_pretty(time_status_change), end='', flush=True)
             time_status_change = time.time()
 
-        time.sleep(1)
+        time.sleep(check_interval)
 
 if __name__ == "__main__":
     main()
